486696523eda997dd026085e6055bc62732fcf1b
Sumair Qaisar <sumairqaisar992@gmail.com>
2022-04-05 16:38:05 +0500

    Update Id column for Animals table

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE ANIMALS (
-    id INT,
+    id INT GENERATED ALWAYS AS IDENTITY ,
     name VARCHAR(100),
     date_of_birth DATE,
     escape_attempts INT,
