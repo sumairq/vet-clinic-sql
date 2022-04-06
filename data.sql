eb175145d504bf9fbf6aedd19bd4fb14bc78a32d
Sumair Qaisar <sumairqaisar992@gmail.com>
2022-04-06 16:09:49 +0500

    Fix error

--- data.sql
+++ data.sql
@@ -1,8 +1,8 @@
 /* Populate database with sample data. */
-INSERT INTO ANIMALS VALUES(1, 'Agumon', '3-FEB-2020', 0, true, 10.23);
+INSERT INTO ANIMALS(name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES('Agumon', '3-FEB-2020', 0, true, 10.23);
 
-INSERT INTO ANIMALS VALUES(2, 'Gabumon', '15-NOV-2018', 2, true, 8);
+INSERT INTO ANIMALS(name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES('Gabumon', '15-NOV-2018', 2, true, 8);
 
-INSERT INTO ANIMALS VALUES(3, 'Pikachu', '7-JAN-2021', 1, false, 15.04);
+INSERT INTO ANIMALS(name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES('Pikachu', '7-JAN-2021', 1, false, 15.04);
 
-INSERT INTO ANIMALS VALUES(4, 'Devimon', '12-MAY-2017', 5, true, 11);
+INSERT INTO ANIMALS(name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES('Devimon', '12-MAY-2017', 5, true, 11);
