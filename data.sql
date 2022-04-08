7a422fff03fd49b3cb7766d09bcfbba1184f2fc4
Sumair <sumairqaisar992@gmail.com>
2022-04-09 00:56:46 +0500

    query multiple tables with joins

--- data.sql
+++ data.sql
@@ -22,3 +22,46 @@ INSERT INTO ANIMALS(name, date_of_birth, escape_attempts, neutered, weight_kg) V
 INSERT INTO ANIMALS(name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES('Blossom', '13-OCT-1998', 3, true, 17);
 
 INSERT INTO ANIMALS(name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES('Ditto', '14-MAY-2022', 4, true, 22);
+
+
+INSERT INTO owners(full_name, age)
+VALUES
+  ('Sam Smith', 34),
+  ('Jennifer Orwell', 19),
+  ('Bob', 45),
+  ('Melody Pond', 77),
+  ('Dean Winchester', 14),
+  ('Jodie Whittaker', 38);
+
+INSERT INTO species(name)
+VALUES
+  ('Pokemon'),
+  ('Digimon');
+
+UPDATE ANIMALS
+SET species_id = 1;
+UPDATE ANIMALS
+SET species_id = 2
+WHERE name LIKE '%mon%';
+
+
+UPDATE ANIMALS
+SET owner_id = 1
+WHERE name = 'Agumon';
+SAVEPOINT OWNER_1;
+UPDATE ANIMALS
+SET owner_id = 2
+WHERE name IN ('Gabumon', 'Pikachu');
+SAVEPOINT OWNER_2;
+UPDATE ANIMALS
+SET owner_id = 3
+WHERE name IN ('Devimon', 'Plantmon');
+SAVEPOINT OWNER_3;
+UPDATE ANIMALS
+SET owner_id = 4
+WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
+SAVEPOINT OWNER_4;
+UPDATE ANIMALS
+SET owner_id = 5
+WHERE name IN ('Angemon', 'Boarmon');
+SAVEPOINT OWNER_5;
