d08ca1ab0f81862b0d7dd864e9b559b1fc775bb0
Sumair <sumairqaisar992@gmail.com>
2022-04-12 20:59:02 +0500

    Introduce new data

--- data.sql
+++ data.sql
@@ -101,3 +101,8 @@ VALUES
   (9, 2, '3-AUG-2020'),
   (10, 3, '24-MAY-2020'),
   (10, 1, '11-JAN-2021');
+
+
+INSERT INTO visits (animal_id, vet_id, date_of_visit) SELECT * FROM (SELECT id FROM animals) animal_ids, (SELECT id FROM vets) vets_ids, generate_series('1980-01-01'::timestamp, '2021-01-01', '4 hours') visit_timestamp;
+
+INSERT INTO owners (full_name, email) select 'Owner ' || generate_series(1,2500000), 'owner_' || generate_series(1,2500000) || '@mail.com';
