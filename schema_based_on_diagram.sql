3eb842a0a8c2b89febd00394a49bce1f9c0f3c23
Sumair <sumairqaisar992@gmail.com>
2022-04-14 03:22:25 +0500

    Add tables for patients, medical_histories, treatments

---
+++ schema_based_on_diagram.sql
@@ -0,0 +1,22 @@
+CREATE TABLE patients(
+	id INT GENERATED ALWAYS AS IDENTITY,
+	name VARCHAR(255),
+	data_of_birth DATE,
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE medical_histories(
+	id INT GENERATED ALWAYS AS IDENTITY,
+	admitted_at DATE,
+	patient_id INT,
+	status VARCHAR(255),
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE treatments(
+	id INT GENERATED ALWAYS AS IDENTITY,
+	type VARCHAR(255),
+	name VARCHAR(255),
+	PRIMARY KEY(id)
+);
+
