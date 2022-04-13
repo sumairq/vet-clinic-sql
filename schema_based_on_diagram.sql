e89ac81408ca099cde637e0a00026e9c891e3328
Dennis Akagha <d.akagha20@gmail.com>
2022-04-13 23:28:36 +0100

    Add the foreign key queries

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -20,3 +20,30 @@ CREATE TABLE treatments(
 	PRIMARY KEY(id)
 );
 
+CREATE TABLE invoice_items(
+	id INT GENERATED ALWAYS AS IDENTITY,
+	unit_price DECIMAL,
+	quantity INT,
+	total_price DECIMAL,
+	invoices_id INT,
+	treatment_id int,
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE invoices(
+	id INT GENERATED ALWAYS AS IDENTITY,
+	total_amount DECIMAL,
+	generated_at DATE,
+	payed_at DATE,
+	medical_history_id INT,
+	PRIMARY KEY(id)
+);
+
+ALTER TABLE invoices ADD CONSTRAINT fk_medical_history FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id);
+
+ALTER TABLE invoice_items ADD CONSTRAINT fk_invoice_id FOREIGN KEY (invoices_id) REFERENCES invoices(id);
+
+ALTER TABLE invoice_items ADD CONSTRAINT fk_treatment_id FOREIGN KEY (treatment_id) REFERENCES treatments(id);
+
+ALTER TABLE medical_histories ADD CONSTRAINT fk_patient_id FOREIGN KEY (patient_id) REFERENCES patients(id);
+
